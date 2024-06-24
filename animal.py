c6ecda728dc12fa9e52ed3f4ae8b7465468bb976
animal.py <littreca@oregonstate.edu>
2024-06-23 18:57:00 -0700

    Done

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Caden Littrell
+# GitHub username: Clittrell13
+# Date: 06/23/2024
+# Description: Asks the user for their favorite animal and then prints out that
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
